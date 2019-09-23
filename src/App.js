57bc1efc7c8b5c7767dad85a2cb5defa3e479fb8
vanely <vanelys94@gmail.com>
2019-09-23 18:44:38 -0400

    graphql frontend api requests

--- src/App.js
+++ src/App.js
@@ -1,13 +1,118 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gql from 'graphql-tag'; // allows us to parse graphql queries/ mutations (has to be bound to component)
+import { graphql } from 'react-apollo'; // makes our app a higher order component, and allows us to make graphql queries/ mutation
+import * as compose from 'lodash.flowright';
 
-function App() {
-  return (
-    <div className="App">
+import './App.scss';
 
-    </div>
-  );
+const TodosQuery = gql`
+query {
+  todos {
+    id
+    text
+    complete
+  }
+}`;
+
+// const CreateMutation = gql`
+// mutation() {
+
+// }`;
+
+const UpdateMutation = gql`
+mutation($id: ID!, $complete: Boolean!)  {
+  updateTodo(id: $id, complete: $complete)
+}`;
+
+// const RemoveMutation = gql`
+// mutation($id: ID!) {
+//   removeTodo(id: $id)
+// }`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: ''
+    };
+  }
+
+  addTodo = () => {
+
+  }
+
+  updateTodo = async (todo) => {
+    await this.props.updateTodo({
+      variables: {
+        id: todo.id,
+        complete: !todo.complete,
+      },
+      update: (store) => {
+        // read cache data(our TodoQuery) for this query
+        const data = store.readQuery({query: TodosQuery});
+        // add mutation change
+        data.todos = data.todos.map((x) => { // since this method will be specific to the todo that I click do I even need to iterate over the todos or should I just make a direct change
+          if (x.id === todo.id) {
+            return {
+              ...todo,
+              complete: !todo.complete,
+            };
+          } else {
+            return x;
+          }
+        });
+        // write data back to cache
+        store.writeQuery({query: TodosQuery, data});
+      }
+    });
+  }
+
+  removeTodo = () => {
+
+  }
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  } 
+
+  render() {
+    // props now have data coming from GraphQl query.
+    console.log(this.props);
+
+    // loading boolean can be used to display loading spinner, and todos array, contains contents of query
+    const {loading, todos} = this.props.data; 
+
+    // represent the loadwith loading boolean
+    if (loading) {
+      return null;
+    }
+
+    // let's iterate and return todos
+    return (
+      <div className="">
+        <div>
+          <h1>Add Todo</h1>
+          <input onChange={this.handleChange} name="todo" type="text" placeholder="enter your todo"/>
+        </div>
+        <h2>Todos</h2>
+        {
+          todos.map((todo) => {
+            return (
+              <div key={todo.id}>
+                <input onChange={ () => this.updateTodo(todo) } type="checkbox" name="complete" checked={todo.complete}/>
+                <strong style={{padding: '0 10px 0 10px'}}>{todo.text}</strong>
+                <span onClick={ () => this.removeTodo }>X</span>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
-export default App;
+// sends the queries and the component
+export default compose(
+  graphql(TodosQuery),
+  graphql(UpdateMutation, {name: "updateTodo"}),
+)(App);
--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import './index.scss';
+
 // connection to the url that out graphql server is running on
 const client = new ApolloClient({
     uri: "http://localhost:4000"
 });
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-import './index.css';
 
 // wrap app in apollo provider allowing access to the client throughout the entire application
 ReactDOM.render(
-<ApolloProvider>
+    <ApolloProvider client={client}>
         <App />
     </ApolloProvider>,
     document.getElementById('root')
